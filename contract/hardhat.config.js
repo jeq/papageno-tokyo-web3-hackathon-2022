996922c909a097a4d249eb54b3675da9de1b5ef4
heku777 <chotips08@gmail.com>
2022-10-30 23:34:50 +0900

    add NFT mit function

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,11 +1,13 @@
 // hardhat.config.js
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
 module.exports = {
   solidity: "0.8.9",
   networks: {
     goerli: {
-      url: "https://eth-goerli.g.alchemy.com/v2/Zq-x0Ek-NkSLCQPQlLpyGQTcMZu9BOYq",
-      accounts: ["a0cc977ec570987be05f2f378797a43ff0c5e8351b28373bada608d5ff102105"],
+      url: process.env.ALCHEMY_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
