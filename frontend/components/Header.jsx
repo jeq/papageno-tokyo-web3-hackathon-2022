00e4b2cf47949a13e17ac669453c9440d88713e2
keitof <keito723g@gmail.com>
2022-10-31 18:01:35 +0900

    fix:revise old-type of Link in Next.js

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -3,10 +3,8 @@ import Link from "next/link";
 export default function HeaderNav() {
   return (
     <nav class="flex items-center justify-between flex-wrap bg-orange-300 p-6">
-      <Link href="/">
-        <a class="flex items-center flex-shrink-0 text-white mr-6">
-          <span class="font-semibold text-xl tracking-tight">Story Box</span>
-        </a>
+      <Link href="/" class="flex items-center flex-shrink-0 text-white mr-6">
+        <span class="font-semibold text-xl tracking-tight">Story Box</span>
       </Link>
       <div class="block lg:hidden">
         <button class="flex items-center px-3 py-2 border rounded text-orange-100 border-orange-400 hover:text-white hover:border-white">
@@ -22,22 +20,25 @@ export default function HeaderNav() {
       </div>
       <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div class="text-sm lg:flex-grow">
-          <Link href="/story/archive/">
-            <a class="block mt-4 lg:inline-block lg:mt-0 text-orange-100 hover:text-white mr-4">
-              ストーリーを読む
-            </a>
+          <Link
+            href="/story/archive/"
+            class="block mt-4 lg:inline-block lg:mt-0 text-orange-100 hover:text-white mr-4"
+          >
+            ストーリーを読む
           </Link>
-          <Link href="/story/tag/">
-            <a class="block mt-4 lg:inline-block lg:mt-0 text-orange-100 hover:text-white mr-4">
-              タグから探す
-            </a>
+          <Link
+            href="/story/tag/"
+            class="block mt-4 lg:inline-block lg:mt-0 text-orange-100 hover:text-white mr-4"
+          >
+            タグから探す
           </Link>
         </div>
         <div>
-          <Link href="/profile/">
-            <a class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-orange-500 hover:bg-white mt-4 lg:mt-0">
-              マイページ
-            </a>
+          <Link
+            href="/profile/"
+            class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-orange-500 hover:bg-white mt-4 lg:mt-0"
+          >
+            マイページ
           </Link>
         </div>
       </div>
