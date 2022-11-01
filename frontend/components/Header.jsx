027984b8ec737994440bd894d8e743de21fb668a
jeq <wrtmyy@yahoo.com>
2022-11-01 21:08:25 +0900

    feat:add archive and single parts

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function HeaderNav() {
   return (
-    <nav class="flex items-center justify-between flex-wrap bg-orange-300 p-6">
-      <Link href="/" class="flex items-center flex-shrink-0 text-white mr-6">
+    <nav class="container mx-auto flex items-center justify-between flex-wrap py-6">
+      <Link href="/" class="flex items-center flex-shrink-0 mr-6 text-gray-800">
         <span class="font-semibold text-xl tracking-tight">Story Box</span>
       </Link>
       <div class="block lg:hidden">
-        <button class="flex items-center px-3 py-2 border rounded text-orange-100 border-orange-400 hover:text-white hover:border-white">
+        <button class="flex items-center px-3 py-2 border rounded text-gray-800 hover:text-gray-800 hover:border-gray-800">
           <svg
             class="fill-current h-3 w-3"
             viewBox="0 0 20 20"
@@ -19,26 +19,29 @@ export default function HeaderNav() {
         </button>
       </div>
       <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div class="text-sm lg:flex-grow">
-          <Link
-            href="/story/archive/"
-            class="block mt-4 lg:inline-block lg:mt-0 text-orange-100 hover:text-white mr-4"
-          >
-            ストーリーを読む
-          </Link>
-          <Link
-            href="/story/tag/"
-            class="block mt-4 lg:inline-block lg:mt-0 text-orange-100 hover:text-white mr-4"
-          >
-            タグから探す
-          </Link>
-        </div>
-        <div>
+        <div class="text-sm lg:flex-grow"></div>
+        <div className="flex items-center">
+          <span className="mr-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+              />
+            </svg>
+          </span>
           <Link
             href="/profile/"
-            class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-orange-500 hover:bg-white mt-4 lg:mt-0"
+            class="inline-block text-sm px-4 py-2 leading-none border rounded text-gray-800 border-gray-800 hover:border-transparent hover:text-orange-500 hover:bg-gray-800 mt-4 lg:mt-0"
           >
-            マイページ
+            ストーリーを書く
           </Link>
         </div>
       </div>
--- frontend/pages/story/single.js
+++ frontend/pages/story/single.js
@@ -3,9 +3,9 @@ import Markdown from "../../components/Markdown";
 
 export default function Archive() {
   return (
-    <div className="min-h-screen bg-gray-50 py-8 flex flex-col justify-center relative overflow-hidden lg:py-12">
-      <div className="relative w-full px-12 py-12 bg-white shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5 md:max-w-3xl md:mx-auto lg:max-w-4xl lg:pt-8 lg:pb-28">
-        <div className="relative w-full py-12 md:max-w-3xl md:mx-auto lg:max-w-4xl lg:pb-16">
+    <div className="container mx-auto flex">
+      <div className="lg:w-7/12">
+        <div className="max-w-full pb-8">
           <img
             src="https://static.vecteezy.com/system/resources/previews/001/882/528/non_2x/beautiful-landscape-pine-forest-with-mesmerizing-mountain-views-free-vector.jpg"
             alt="Mountain"
@@ -15,6 +15,26 @@ export default function Archive() {
           ></img>
         </div>
         <Markdown></Markdown>
+      </div>
+      <div className="lg:w-5/12 pl-6">
+        <div className="font-bold">このストーリーを書いた人</div>
+        <span className="mr-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-40 h-40"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+        </span>
+        <div>宮沢賢治</div>
       </div>
     </div>
   );
