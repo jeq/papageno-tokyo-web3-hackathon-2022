9f4cd20fb9b9d376a7ca4923ee8e4045173d348f
jeq <wrtmyy@yahoo.com>
2022-11-02 17:50:37 +0900

    feat:add editor

---
+++ frontend/components/editor/Index.jsx
@@ -0,0 +1,44 @@
+import { Editor as DraftEditor, EditorState, RichUtils } from "draft-js";
+import "draft-js/dist/Draft.css";
+import React, { useState } from "react";
+
+function myBlockStyleFn(contentBlock) {
+  const type = contentBlock.getType();
+
+  switch (type) {
+    case "blockquote":
+      return "px-4 py-2 border-l-4 bg-neutral-100 text-neutral-600 border-neutral-300 quote not-italic";
+  }
+
+  return "";
+}
+
+const Editor = () => {
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+
+  const handleBold = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"));
+  };
+
+  return (
+    <div className="m-4">
+      <div className="mb-1 pr-4 flex items-center mx-auto prose prose-stone">
+        <div className="ml-auto flex items-center space-x-5">
+          <button onClick={handleBold}>B</button>
+        </div>
+      </div>
+      <div className="shadow-sm border border-gray-300 rounded-md sm:text-sm overflow-scroll h-[500px] p-3 prose prose-stone mx-auto">
+        <DraftEditor
+          editorState={editorState}
+          onChange={setEditorState}
+          placeholder="Tell a story..."
+          blockStyleFn={myBlockStyleFn}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Editor;
