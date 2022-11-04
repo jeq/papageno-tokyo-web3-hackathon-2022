a765abecf927aa0045af62937aa46fe894597374
heku777 <chotips08@gmail.com>
2022-11-04 23:26:07 +0900

    add Like function & profile biography

--- contract/contracts/Profile.sol
+++ contract/contracts/Profile.sol
@@ -7,6 +7,7 @@ contract Profile {
     struct UserProfile {
         string name; // username
         string avatar; // user avator -> あらかじめ数枚Contract側で用意する？
+        string biography; // profile
         address walletAddress;
     }
 
@@ -16,22 +17,32 @@ contract Profile {
 
     mapping(address=>uint256) public addressTouserId; // アドレスとユーザーIDの紐付け
 
-    function createUserProfile(string memory _name, string memory _avatar) external {
+    function createUserProfile(
+        string memory _name,
+        string memory _avatar,
+        string _biography
+    ) external {
         require(addressTouserId[msg.sender] == 0, "An account already exists.");
-        UserProfile memory _newUserProfile = UserProfile(_name, _avatar, msg.sender);
+        UserProfile memory _newUserProfile = UserProfile(_name, _avatar, _biography, msg.sender);
         userProfile.push(_newUserProfile);
         addressTouserId[msg.sender] = userId;
         userId ++;
     }
 
-    function editUserProfile(string memory _name, string memory _avatar) external {
+    function editUserProfile(string memory _name, string memory _avatar, string memory _biography) external {
         require(addressTouserId[msg.sender] > 0, "You have not yet registered a profile.");
         uint256 _userId = addressTouserId[msg.sender] - 1;
         userProfile[_userId].name = _name;
         userProfile[_userId].avatar = _avatar;
+        userProfile[_userId].biography = _biography;
     }
 
-    function getUserProfile() external view returns(string memory, string memory, address){
+    function getUserProfile() external view returns(
+        string memory,
+        string memory,
+        string memory,
+        address
+    ){
         require(addressTouserId[msg.sender] > 0, "You have not yet registered a profile.");
         uint256 _userId = addressTouserId[msg.sender] - 1;
         return (userProfile[_userId].name, userProfile[_userId].avatar, userProfile[_userId].walletAddress);
--- contract/contracts/RecoveryStory.sol
+++ contract/contracts/RecoveryStory.sol
@@ -5,6 +5,9 @@ pragma solidity ^0.8.9;
 
 
 contract RecoveryStory {
+    constructor() {
+        uint basePrice = 0.001;
+    }
     struct Story {
         string storyTitle; //タイトル
         string[] tags; //タグ
@@ -15,11 +18,13 @@ contract RecoveryStory {
         uint numLike; //likeの数
         uint itemId;  //storyのID
         address authorAddress;  //著者のアドレス
+        address[] likeUserAdress; // いいねしたアドレスの配列
     }
 
     struct UserProfile {
         string name; // username
         string avatar; // user avator -> あらかじめ数枚Contract側で用意する？
+        string biography; // profile
         address walletAddress;
     }
 
@@ -36,25 +41,38 @@ contract RecoveryStory {
     mapping(string=>address) titleToAddress; // 作品タイトルと所有者アドレスの紐づけ
     mapping(string=>uint) titleToStoryId; // 作品タイトルと作品IDの紐づけ
 
-    function createUserProfile(string memory _name, string memory _avatar) external {
+    function createUserProfile(
+        string memory _name,
+        string memory _avatar,
+        string memory _biography
+    ) external {
         require(addressTouserId[msg.sender] == 0, "An account already exists.");
-        UserProfile memory _newUserProfile = UserProfile(_name, _avatar, msg.sender);
+        UserProfile memory _newUserProfile = UserProfile(_name, _avatar, _biography, msg.sender);
         userProfile.push(_newUserProfile);
         addressTouserId[msg.sender] = userId;
         userId ++;
     }
 
-    function editUserProfile(string memory _name, string memory _avatar) external {
+    function editUserProfile(string memory _name, string memory _avatar, string memory _biography) external {
         require(addressTouserId[msg.sender] > 0, "You have not yet registered a profile.");
         uint256 _userId = addressTouserId[msg.sender] - 1;
         userProfile[_userId].name = _name;
         userProfile[_userId].avatar = _avatar;
+        userProfile[_userId].biography = _biography;
     }
 
-    function getUserProfile() external view returns(string memory, string memory, address){
+    function getUserProfile() external view returns(
+        string memory,
+        string memory,
+        string memory,
+        address
+    ){
         require(addressTouserId[msg.sender] > 0, "You have not yet registered a profile.");
         uint256 _userId = addressTouserId[msg.sender] - 1;
-        return (userProfile[_userId].name, userProfile[_userId].avatar, userProfile[_userId].walletAddress);
+        return (userProfile[_userId].name,
+                userProfile[_userId].avatar,
+                userProfile[_userId].biography,
+                userProfile[_userId].walletAddress);
     }
 
     function getAuthor(address _authorAddress) public view returns(string memory) {
@@ -68,8 +86,9 @@ contract RecoveryStory {
         string[] memory tags,
         string memory _storyBody,
         string memory _icatchSvg
-      ) public {
+    ) public {
         require(titleToStoryId[_storyTitle] == 0, "An Story Data already exists.");
+        address[] memory _emptyLikeUserAdress;
         Story memory _newStory = Story(
             _storyTitle,
             tags,
@@ -79,7 +98,8 @@ contract RecoveryStory {
             block.timestamp,
             0,
             storyIdCounter,
-            msg.sender
+            msg.sender,
+            _emptyLikeUserAdress
             );
         story.push(_newStory);
         titleToAddress[_storyTitle] = msg.sender;
@@ -91,8 +111,7 @@ contract RecoveryStory {
         string memory _storyTitle,
         string[] memory tags,
         string memory _storyBody,
-        string memory _icatchSvg,
-        uint _numLike
+        string memory _icatchSvg
     ) external {
         require(titleToStoryId[_storyTitle] > 0, "You have not yet registered a sotry data.");
         uint256 _storyId = titleToStoryId[_storyTitle] - 1;
@@ -101,11 +120,6 @@ contract RecoveryStory {
         story[_storyId].storyBody = _storyBody;
         story[_storyId].icatchSvg = _icatchSvg;
         story[_storyId].updateDate = block.timestamp;
-        story[_storyId].numLike = _numLike ;
-    }
-
-    function buyNft(string memory _storyTitle) external {
-        titleToAddress[_storyTitle] = msg.sender;
     }
 
     function getStory(string memory _storyTitle) external view returns (
@@ -114,8 +128,14 @@ contract RecoveryStory {
         string memory,
         string memory,
         string memory,
-        uint, uint, uint, uint, address
+        uint,
+        uint,
+        uint,
+        uint,
+        address,
+        address[] memory
       ) {
+        require(titleToStoryId[_storyTitle] > 0, "No stories with the specified title are registered.");
         uint256 _storyId = titleToStoryId[_storyTitle] - 1;
         string memory storyAuthor = getAuthor(story[_storyId].authorAddress);
         return (
@@ -128,10 +148,38 @@ contract RecoveryStory {
             story[_storyId].updateDate,
             story[_storyId].numLike,
             story[_storyId].itemId,
-            story[_storyId].authorAddress
+            story[_storyId].authorAddress,
+            story[_storyId].likeUserAdress
         );
+    }
+
+    function addLike(string memory _storyTitle) external {
+        require(titleToStoryId[_storyTitle] > 0, "No stories with the specified title are registered.");
+        uint256 _storyId = titleToStoryId[_storyTitle] - 1;
+        require(story[_storyId].authorAddress != msg.sender, "Story authors cannot be liked.");
+        bool checkDoubleLike = false;
+        for (uint index=0; index < story[_storyId].likeUserAdress.length; index++) {
+            if (story[_storyId].likeUserAdress[index] == msg.sender) {
+                checkDoubleLike = true;
+            }
+        }
+        require(checkDoubleLike == false, "You are liking this story on the nest.");
+        story[_storyId].numLike ++;
+        story[_storyId].likeUserAdress.push(msg.sender);
+    }
+
+    function getLikeNum(string memory _storyTitle) public returns (string memory) {
+        require(titleToStoryId[_storyTitle] > 0, "No stories with the specified title are registered.");
+        uint256 _storyId = titleToStoryId[_storyTitle] - 1;
+        return (story[_storyId].numLike);
+    }
+
+    function buyNft(string memory _storyTitle, uint _payNum) external {
+        uint price = getLikeNum(_storyTitle);
+        require(_payNum >= price, "There are not enough funds for the price.");
+        titleToAddress[_storyTitle] = msg.sender;
     }
 }
 
-// "cardene", "cardene avatar"
+// "cardene", "cardene avatar", "cardene profile"
 // "cardene",["A","B"],"cardene story","cardene svg"
