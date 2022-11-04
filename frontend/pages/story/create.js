600e754e784284ecf96200877eac1dd6da0fb3f4
jeq <wrtmyy@yahoo.com>
2022-11-04 14:04:20 +0900

    fix:split tags with comma

--- frontend/pages/story/create.js
+++ frontend/pages/story/create.js
@@ -104,6 +104,8 @@ export default function Create() {
           className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
         />
       </div>
+      {console.log(valueTags.split(","))}
+      {console.log(valueTags)}
 
       <div className="sm:col-span-2">
         <label className="inline-block text-gray-800 text-sm sm:text-base mb-2">
